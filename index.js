dfa3c2396392f52ad9eecec689115ab9858eb0dd
Nguyễn Văn Phước <phuoc.nguyen@be.com.vn>
2022-11-21 22:44:14 +0700

    fix syntax

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const { Component, } = React;
+const ReactNative = require('react-native');
 const { ViewPropTypes,  } = require('deprecated-react-native-prop-types');
 const createReactClass = require('create-react-class');
 const PropTypes = require('prop-types');
